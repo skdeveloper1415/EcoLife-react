279371d7c0bb143cf36d6f176183620c26df78a5
skdeveloper1415 <khan446688@gmail.com>
2025-08-09 15:36:54 +0530

    update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -51,18 +51,18 @@ const Testimonials = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
         }
       },
       {
